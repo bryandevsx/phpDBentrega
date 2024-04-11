cb144a70e53392c378dbeb4dfa8ba80ee24fbc6f
Bryan Teixeira <61585035+bryandevsx@users.noreply.github.com>
2024-04-10 21:11:17 -0300

    entrega

---
+++ simplecrud/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Cadastro</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Sistema de Cadastro</h1>
+        <p><a href="cadastro.php" class="btn btn-primary">Clique aqui para incluir uma nova mensagem!</a></p>
+        <p><a href="lista.php" class="btn btn-secondary">Clique aqui para ver a listagem de dados!</a></p>
+    </div>
+</body>
+</html>
